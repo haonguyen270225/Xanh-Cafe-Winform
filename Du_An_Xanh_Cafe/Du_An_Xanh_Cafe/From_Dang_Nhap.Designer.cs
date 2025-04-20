246170b564f1a6efa34f78fb28eca7d2c2c1b0f4
haonguyennhat <haonguyennhat270225@gmail.com>
2025-04-21 02:51:28 +0700

    update code Form_Dang_Nhap branch Dang_Nhap_Test

--- Du_An_Xanh_Cafe/Du_An_Xanh_Cafe/From_Dang_Nhap.Designer.cs
+++ Du_An_Xanh_Cafe/Du_An_Xanh_Cafe/From_Dang_Nhap.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "From_Dang_Nhap";
+            SuspendLayout();
+            // 
+            // From_Dang_Nhap
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "From_Dang_Nhap";
+            Text = "From_Dang_Nhap";
+            Load += From_Dang_Nhap_Load;
+            ResumeLayout(false);
         }
 
         #endregion
