ef3bde9539ec196f9cbc5a631a18aa7b5df7a2bb
haonguyennhat <haonguyennhat270225@gmail.com>
2025-04-21 02:04:57 +0700

    Add form Dang_Nhap

---
+++ Du_An_Xanh_Cafe/Du_An_Xanh_Cafe/From_Dang_Nhap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Du_An_Xanh_Cafe
+{
+    public partial class From_Dang_Nhap : Form
+    {
+        public From_Dang_Nhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
